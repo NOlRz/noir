b5d5c881249705698e07ca1ff1e5f899787a1c8d
NOlRz <fizpelobot@gmail.com>
2025-04-28 22:57:29 -0300

    test

---
+++ js/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/757-dc6ca04e4813ff42.js","static/chunks/pages/index-27af309e00a0d6ae.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
